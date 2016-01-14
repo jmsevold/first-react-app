836a3c7abd9c14db7ac76e04f7018158bf2be493
Jonathan Sevold <jmsevold@gmail.com>
2016-01-13 18:17:57 -0800

    bullshit changes

--- form.js
+++ form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Form extends React.Component{
+class Form extends React.Component {
   constructor(){
     super();
     this.state = {
@@ -10,7 +10,7 @@ class Form extends React.Component{
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(){
+ handleSubmit(){
     let firstName = this.refs.firstName.value;
     let lastName  = this.refs.lastName.value;
     let person = {firstName: firstName, lastName: lastName};
@@ -21,43 +21,52 @@ class Form extends React.Component{
       names: this.state.names
     });
     console.log(person);
-
-
   }
- 
+
   render(){
     return(
       <div>
         <input type="text" ref="firstName" />
         <input type="text" ref="lastName" />
         <button onClick={this.handleSubmit}> Submit</button>
-        <List people={this.state.names}/>
+        <List/>
       </div>
     ); 
   }
-
 }
 
 
-var List = (props) =>{
-  unmount(){
-    ReactDOM.unmountComponentAtNode('person');
-  }
+var List = (props) => {
+  // constructor(){
+  //   super();
+  // }
 
-  let names = props.people.map((person)=>{
+  // unmount(){
+  //   ReactDOM.unmountComponentAtNode('person');
+  // }
+ 
+  render(){
+    let names = props.people.map((person)=>{
     return(
       <div id="person">
         <li> {person.firstName} {person.lastName} is a great guy</li>
-        <button onClick={this.unmount}>remove</button>
+        <button>remove</button>
       </div>
     );
-  });
+  }
 
   return(
-      <ul>
-        {names}
-      </ul>
-    ); 
+    <ul>
+      {names}
+    </ul>
+  );
+
 }
+
+
+
+
+
+
 
 export default Form
